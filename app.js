8e4ae52e4ee42b9816c5f82ac1fd338e5fc793d1
Your Name <you@example.com>
2022-05-11 12:08:03 +0200

    primer connit

---
+++ app.js
@@ -0,0 +1,12 @@
+//window.alert("hola");
+//windows.console.log("hola2")
+
+const objetoInput = windows.document.querySelector("input");
+const objetoUl= documento.querySelector("ul");
+
+//console.log(objetoInput.value);
+
+function pintar(){
+    let nuevoNodo = `<li>${objetoIput.value}</li>
+    objetoUl.innerHTML+=nuevoNodo;`
+}
